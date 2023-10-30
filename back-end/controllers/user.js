0dc1c944a25236157f009e046c77e93a252ad2dd
Caroline Protat <caroline.protat@hotmail.fr>
2023-10-30 17:12:36 +0100

    ajustement du controllers signUp pour ajouter des conditions d'inscription : adresse email valide et mot de passe +8 caractères. Et ajout de la vérification que l'adresse email renseignée à l'inscription ne soit pas déjà enregistrée dans la base de données

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -1,19 +1,57 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
+// TEST VALIDITÉ FORMAT EMAIL
+
+const isValidEmail = (email) => {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(email); // renvoie true ou false
+};
+
+// MIDDLEWARE INSCRIPTION
+// **********************
+
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch((error) => res.status(400).json({ error }));
+// Valider le format de l'adresse email et la longueur du mot de passe
+  if (!isValidEmail(req.body.email) || req.body.password.length < 8) {
+    console.log('Inscription impossible : email ou mot de passe invalide');
+    return res.status(400).json({ error: 'Inscription impossible : email ou mot de passe invalide' });
+  }
+
+  // Vérifier que l'adresse mail n'est pas déjà enregistrée dans la base de données
+  User.findOne({ email: req.body.email })
+
+    .then((existingUser) => {
+      // Si email existe déjà
+      if (existingUser) {
+        console.log('Adresse email déjà enregistrée');
+        return res.status(409).json({ error: 'Adresse email déjà enregistrée' });
+      }
+
+      // Si email n'existe pas : inscription nouveau compte
+      bcrypt.hash(req.body.password, 10)
+        .then((hash) => {
+          const user = new User({ // création nouveau client
+            email: req.body.email,
+            password: hash,
+          });
+          user.save() // Pour sauvegarder dans la base de données
+            .then(() => {
+              console.log('Inscription nouveau client RÉUSSIE !');
+              return res.status(201).json({ message: 'Utilisateur créé !' });
+            })
+            .catch((error) => res.status(400).json({ error }));
+        })
+        .catch((error) => res.status(500).json({ error }));
     })
+
     .catch((error) => res.status(500).json({ error }));
 };
+
+// MIDDLEWARE CONNEXION
+// ********************
 
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
