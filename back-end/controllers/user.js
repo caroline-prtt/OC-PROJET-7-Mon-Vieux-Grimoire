0fc215fd3cea7db93cc307bbff1c322012d0d83c
Caroline Protat <caroline.protat@hotmail.fr>
2023-10-31 11:43:49 +0100

    finalisation controllers login

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 /* eslint-disable no-unused-vars */
 const bcrypt = require('bcrypt');
@@ -44,15 +45,15 @@ exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (user === null) {
-        res.status(401).json({ message: 'Paire identifiant/mot de passe incorrect' });
+        res.status(401).json({ message: 'Utilisateur non trouvé !' });
       } else {
         bcrypt.compare(req.body.password, user.password)
           .then((valid) => {
             if (!valid) {
-              res.status(401).json({ message: 'Paire identifiant/mot de passe incorrect' });
+              res.status(401).json({ message: 'Mot de passe incorrect' });
             } else {
               res.status(200).json({
-                userId: user.id,
+                userId: user._id,
                 token: jwt.sign(
                   { userId: user._id },
                   'RANDOM_TOKEN_SECRET',
