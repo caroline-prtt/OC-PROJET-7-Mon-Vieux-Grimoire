7381c5d726d6486cb05a179343007c8f9553fdae
Caroline Protat <caroline.protat@hotmail.fr>
2023-11-21 11:01:08 +0100

    ajout de commentaires dans le fichier server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,8 @@
-const http = require('http');
-const app = require('./app');
+const http = require('http'); // importation du module HTTP de Node
+const app = require('./app'); // importation de l'application Express définie par le fichier app.js
 
+// NORMALISATION DU PORT
+// Fonction pour s'assurer que le port sur lequel le serveur écoute est correctement défini et normalisé
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
 
@@ -14,12 +16,16 @@ const normalizePort = (val) => {
   return false;
 };
 
+// CONFIGURATION DU PORT sur lequel le serveur écoutera les connexion
+// Utilise la valeur du port définit dans la variable d'environnement OU le port 4000 par défaut
 const port = normalizePort(process.env.PORT || '4000');
 
 app.set('port', port);
 
+// CRÉATION DU SERVEUR avec notre application comme argument
 const server = http.createServer(app);
 
+// GESTION DES ERREURS de mise en écoute du serveur
 const errorHandler = (error) => {
   if (error.syscall !== 'listen') {
     throw error;
@@ -40,7 +46,10 @@ const errorHandler = (error) => {
   }
 };
 
+// ECOUTE DU SERVEUR :
+// En cas d'erreur, la fonction errorHandler est appelée
 server.on('error', errorHandler);
+// Si succès, affiche message indiquant l'adresse sur laquelle le serveur écoute
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
