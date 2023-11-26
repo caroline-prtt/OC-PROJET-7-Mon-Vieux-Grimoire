ecbf1d6b3f90a6efb02ff7ed4d96b6374c081224
Caroline Protat <caroline.protat@hotmail.fr>
2023-11-26 17:32:32 +0100

    ajout de commentaires dans le middleware sharp

--- back-end/middleware/sharp-config.js
+++ back-end/middleware/sharp-config.js
@@ -2,14 +2,17 @@ const sharp = require('sharp');
 const fs = require('fs');
 
 const resizeAndConvertToWebp = (req, res, next) => {
+  // Vérifie si un fichier est téléchargé
   if (!req.file) {
     return next();
   }
 
+  // Utilisation de sharp pour redimensionner et convertir en WebP
   sharp(req.file.path)
     .resize(206, 260, { fit: 'cover' }) // Applique un object-fit cover
     .toFormat('webp')
     .toFile('images/' + req.file.filename.replace(/\.(jpg|jpeg|png)$/, '.webp'))
+    // Une fois que ces opérations sont terminées, on exécute le .then
     .then(() => {
       // Modification du nom de fichier avec l'extension WebP
       req.file.filename = req.file.filename.replace(/\.(jpg|jpeg|png)$/, '.webp');
