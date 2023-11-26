690bcb0b3368360d5cd96d0f7e996e4d37b3a917
Caroline Protat <caroline.protat@hotmail.fr>
2023-11-26 15:50:21 +0100

    ajout commentaires au middleware auth.js

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const { userId } = decodedToken;
-    req.auth = {
+    const token = req.headers.authorization.split(' ')[1]; // récupère le token après Bearer et espace
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // on vérifie validité token et on le décode : renvoie le payload cad l'id utilisateur
+    const { userId } = decodedToken; // Stocke l'id récupéré
+    req.auth = { // Ajoute les information d'authentification à l'objet request
       userId,
     };
     next();
