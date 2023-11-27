7df7a17f2464684affa7e7387ce610aa1896ee7b
Caroline Protat <caroline.protat@hotmail.fr>
2023-11-27 20:04:36 +0100

    ajout de commentaires aux controllers de books.js

--- back-end/controllers/books.js
+++ back-end/controllers/books.js
@@ -10,15 +10,18 @@ exports.getAllBooks = async (req, res, next) => {
 
 // AJOUT D'UN NOUVEAU LIVRE
 exports.createBook = (req, res, next) => {
+  // extrait le contenu du champ book de la requête et convertit le JSON en objet JS
   const bookObject = JSON.parse(req.body.book);
+  // supprime certaines propriétés non utilisées lors de la création d'un livre
   delete bookObject._id;
   delete bookObject._userId;
+  // création nouvelle instance du modèle Book - combine les propriétés de bookObject avec l'ID de l'utilisateur et l'URL de l'image
   const book = new Book({
     ...bookObject,
     userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
   });
-
+  // Enregistrement du livre dans la base de données
   book.save()
     .then(() => { res.status(201).json({ message: 'Objet enregistré !' }); })
     .catch((error) => { res.status(400).json({ error }); });
@@ -33,13 +36,19 @@ exports.getOneBook = async (req, res, next) => {
 
 // MODIFICATION D'UN LIVRE
 exports.modifyBook = (req, res, next) => {
+  // Objet contenant les informations du livre à mettre à jour
+  // Si nouvelle image, l'URL est mise à jour
+  // Sinon objet créé avec les informations du corps de la requête
   const bookObject = req.file ? {
     ...JSON.parse(req.body.book),
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
   } : { ...req.body };
+  // supprime la propriété _userId de l'objet bookObject pour éviter de modifier l'ID de l'utilisateur associé au livre
   delete bookObject._userId;
+  // Recherche le livre spécifique à l'aide de son id
   Book.findOne({ _id: req.params.id })
     .then((book) => {
+      // vérifie si l'ID de l'utilisateur associé au livre correspond à l'ID de l'utilisateur authentifié
       if (book.userId !== req.auth.userId) {
         res.status(403).json({ message: 'Unauthorized request' });
       } else {
@@ -50,7 +59,9 @@ exports.modifyBook = (req, res, next) => {
           .then(() => {
             // supprime l'ancienne image si une nouvelle image est téléchargée
             if (req.file) {
+              // divise l'URL de l'ancienne image en utilisant "/images/" comme délimiteur et récupère le deuxième élément du tableau ainsi créé, donc le nom du fichier
               const filename = oldImageUrl.split('/images/')[1];
+              // supprime le fichier spécifié
               fs.unlink(`images/${filename}`, (err) => {
                 if (err) {
                   console.error('Error deleting old image:', err);
@@ -69,11 +80,15 @@ exports.modifyBook = (req, res, next) => {
 exports.deleteBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then((book) => {
+      // vérifie si l'ID de l'utilisateur associé au livre correspond à l'ID de l'utilisateur authentifié
       if (book.userId !== req.auth.userId) {
         res.status(403).json({ message: 'Unauthorized request' });
       } else {
+        // divise l'URL de l'ancienne image en utilisant "/images/" comme délimiteur et récupère le deuxième élément du tableau ainsi créé, donc le nom du fichier
         const filename = book.imageUrl.split('/images/')[1];
+        // suppression de l'image spécifiée
         fs.unlink(`images/${filename}`, () => {
+          // Après avoir supprimé le fichier image, on supprime le livre de la base de données en utilisant son ID
           Book.deleteOne({ _id: req.params.id })
             .then(() => { res.status(200).json({ message: 'Livre supprimé !' }); })
             .catch((error) => res.status(401).json({ error }));
@@ -87,6 +102,9 @@ exports.deleteBook = (req, res, next) => {
 
 // RÉCUPÉRATION DES 3 LIVRES LES MIEUX NOTÉS
 exports.getBestBooks = (req, res, next) => {
+  // recherche dans tous les livres
+  // trie les résultats en fonction de la propriété averageRating par ordre décroissant donc les mieux notés en premier
+  // limité à 3 résultats
   Book.find().sort({ averageRating: -1 }).limit(3)
     .then((books) => res.status(200).json(books))
     .catch((error) => res.status(400).json({ error }));
